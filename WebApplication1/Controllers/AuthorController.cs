1f6df8cf5b665a4bf1b5187e729bafa68a570fea
rvng3301 <artembilous7@gmail.com>
2024-05-19 16:34:30 +0300

    Initial commit

---
+++ WebApplication1/Controllers/AuthorController.cs
@@ -0,0 +1,13 @@
+using System.Web.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class AuthorController : Controller
+    {
+        // GET: Author
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
